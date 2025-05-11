97eb8f189405d1a1f903619839f74f506594b74a
sohee <soheeaubrey@ewha.ac.kr>
2025-05-12 04:09:38 +0900

    feature: connecting font and color to theme

--- app/src/main/java/org/sopt/collaboration/campuspick/core/designsystem/theme/Theme.kt
+++ app/src/main/java/org/sopt/collaboration/campuspick/core/designsystem/theme/Theme.kt
@@ -1,57 +1,62 @@
 package org.sopt.collaboration.campuspick.core.designsystem.theme
 
-import android.os.Build
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+object CampuspickTheme {
+    val colors: CampuspickColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalCampuspickColorsProvider.current
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+    val typography: CampuspickTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalCampuspickTypographyProvider.current
+}
 
 @Composable
-fun MyApplicationTheme(
+fun ProvideCampuspickColorsAndTypography(
+    colors: CampuspickColors,
+    typography: CampuspickTypography,
+    content: @Composable () -> Unit
+){
+    CompositionLocalProvider(
+        LocalCampuspickColorsProvider provides colors,
+        LocalCampuspickTypographyProvider provides typography,
+        content = content
+    )
+}
+
+@Composable
+fun CampuspickTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    ProvideCampuspickColorsAndTypography(
+        colors = defaultCampuspickColors,
+        typography = defaultCampuspickTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode){
+            SideEffect {
+                (view.context as Activity).window.run {
+                    WindowCompat.getInsetsController(this,view).isAppearanceLightStatusBars = false
+                }
+            }
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
