c943c28dd375ad4152c40cdd4dfe01fa77cc1cc7
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-05-12 22:08:27 +0900

    feature: Add focus cleaner

--- app/src/main/java/org/sopt/collaboration/campuspick/core/ui/extension/ModifierExt.kt
+++ app/src/main/java/org/sopt/collaboration/campuspick/core/ui/extension/ModifierExt.kt
@@ -3,12 +3,15 @@ package org.sopt.collaboration.campuspick.core.ui.extension
 import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 
 @SuppressLint("ModifierFactoryUnreferencedReceiver")
 @OptIn(ExperimentalFoundationApi::class)
@@ -43,5 +46,13 @@ fun Modifier.customClickable(
             } else null,
             interactionSource = remember { MutableInteractionSource() }
         )
+    }
+}
+
+fun Modifier.addFocusCleaner(focusManager: FocusManager): Modifier {
+    return this.pointerInput(Unit) {
+        detectTapGestures(onTap = {
+            focusManager.clearFocus()
+        })
     }
 }
