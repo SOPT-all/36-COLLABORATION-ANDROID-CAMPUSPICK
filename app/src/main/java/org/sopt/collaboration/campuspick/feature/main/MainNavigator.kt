e5cfcb78f619d08e3fdda20935a1603e54b6a844
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-05-14 19:45:30 +0900

    refactor: Edit startDestination

--- app/src/main/java/org/sopt/collaboration/campuspick/feature/main/MainNavigator.kt
+++ app/src/main/java/org/sopt/collaboration/campuspick/feature/main/MainNavigator.kt
@@ -24,7 +24,7 @@ internal class MainNavigator(
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
-    val startDestination = Route.Search
+    val startDestination = MainTab.HOME.route
 
     val currentTab: MainTab?
         @Composable get() = MainTab.find { tab ->
