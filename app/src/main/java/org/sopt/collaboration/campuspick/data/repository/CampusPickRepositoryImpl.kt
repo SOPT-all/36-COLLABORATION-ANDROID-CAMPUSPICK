4f867dac6688d90d3cc6bbe008b35828be619edf
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-05-20 21:05:20 +0900

    feature: Add repository

---
+++ app/src/main/java/org/sopt/collaboration/campuspick/data/repository/CampusPickRepositoryImpl.kt
@@ -0,0 +1,53 @@
+package org.sopt.collaboration.campuspick.data.repository
+
+import org.sopt.collaboration.campuspick.data.api.CampusPickService
+import org.sopt.collaboration.campuspick.domain.model.FilteredClub
+import org.sopt.collaboration.campuspick.domain.repository.CampusPickRepository
+
+class CampusPickRepositoryImpl(
+    private val campusPickService: CampusPickService
+) : CampusPickRepository {
+    override suspend fun getPopularClubs() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPopularActivities() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRankClubs() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getSearchClubs(
+        title: String?,
+        category: String?,
+        deadlineType: String?,
+        region: String?,
+        clubDay: String?
+    ): Result<List<FilteredClub>> = runCatching {
+        campusPickService.getSearchClubs(
+            title = title,
+            category = category,
+            deadlineType = deadlineType,
+            region = region,
+            clubDay = clubDay
+        ).data
+    }.mapCatching {
+        it.map {
+            FilteredClub(
+                id = it.clubInfo.id,
+                name = it.clubInfo.name,
+                image = it.clubInfo.image,
+                category = it.clubInfo.image,
+                region = it.clubInfo.region,
+                clubDay = it.clubInfo.clubDay,
+                postTitle = it.recruitPost.title,
+                postImage = it.recruitPost.image,
+                viewCount = it.recruitPost.viewCount,
+                commentCount = it.recruitPost.commentCount,
+                dDay = it.recruitPost.dDay
+            )
+        }
+    }
+}
