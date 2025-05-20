a5cd97287721b7f4dce28c502bc20fcab28b0778
sohee <144779368+sohee6989@users.noreply.github.com>
2025-05-20 15:44:55 +0900

    refactor: Add fillMaxWidth at Image

--- app/src/main/java/org/sopt/collaboration/campuspick/core/designsystem/component/card/CampuspickCard.kt
+++ app/src/main/java/org/sopt/collaboration/campuspick/core/designsystem/component/card/CampuspickCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -46,6 +47,7 @@ fun CampuspickCard(
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .height(imageHeight)
+                .fillMaxWidth()
         )
 
         CardContent(
