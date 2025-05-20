1c4d7df4f6bf9bd647158f49c4859c93e01f4260
sohee <144779368+sohee6989@users.noreply.github.com>
2025-05-20 23:08:51 +0900

    refactor: Edit card detail

--- app/src/main/java/org/sopt/collaboration/campuspick/core/designsystem/component/card/CampuspickCard.kt
+++ app/src/main/java/org/sopt/collaboration/campuspick/core/designsystem/component/card/CampuspickCard.kt
@@ -1,26 +1,35 @@
 package org.sopt.collaboration.campuspick.core.designsystem.component.card
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import org.sopt.collaboration.campuspick.R
 import org.sopt.collaboration.campuspick.core.designsystem.theme.CampuspickTheme
+import org.sopt.collaboration.campuspick.core.ui.preview.DefaultPreview
 
 @Composable
 fun CampuspickCard(
@@ -37,28 +46,32 @@ fun CampuspickCard(
     Card(
         modifier = modifier
             .width(155.dp)
-            .height(172.dp)
-            .clip(RoundedCornerShape(6.dp))
-            .background(cardBackgroundColor)
+            .clip(RoundedCornerShape(6.dp)),
+        colors = CardDefaults.cardColors(
+            contentColor = cardBackgroundColor
+        )
     ) {
-        Image(
-            painter = cardImage,
-            contentDescription = "card image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .height(imageHeight)
-                .fillMaxWidth()
-        )
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = cardImage,
+                contentDescription = "card image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(imageHeight)
+                    .fillMaxWidth()
+            )
 
-        CardContent(
-            cardTitle = cardTitle,
-            cardContentHorizontalPadding = cardContentHorizontalPadding,
-            cardContentVerticalPadding = cardContentVerticalPadding,
-            titleMetaSpacer = titleMetaSpacer,
-            metaInfoContent = metaInfoContent,
-            modifier = Modifier
-        )
-
+            CardContent(
+                cardTitle = cardTitle,
+                cardContentHorizontalPadding = cardContentHorizontalPadding,
+                cardContentVerticalPadding = cardContentVerticalPadding,
+                titleMetaSpacer = titleMetaSpacer,
+                metaInfoContent = metaInfoContent,
+                modifier = Modifier
+            )
+        }
     }
 }
 
@@ -89,5 +102,53 @@ private fun CardContent(
         Spacer(modifier = titleMetaSpacer)
 
         metaInfoContent()
+    }
+}
+
+@DefaultPreview
+@Composable
+private fun CampuspickCardPreview() {
+    CampuspickTheme {
+        CampuspickCard(
+            cardBackgroundColor = CampuspickTheme.colors.Gray5,
+            cardImage = painterResource(R.drawable.img_activity_lisolab),
+            imageHeight = 155.dp,
+            cardTitle = "⭐\uFE0F꿈꾸는 과학 38⭐\uFE0F 신입부원을 상시 모집합니다!",
+            cardContentHorizontalPadding = 6.dp,
+            cardContentVerticalPadding = 4.dp,
+            titleMetaSpacer = Modifier.height(6.dp),
+            metaInfoContent = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Text(
+                        text = "D-67",
+                        color = CampuspickTheme.colors.Gray2,
+                        style = CampuspickTheme.typography.caption3
+                    )
+
+                    Spacer(modifier = Modifier.width(4.dp))
+
+                    Image(
+                        modifier = Modifier
+                            .size(12.dp),
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_post_shown),
+                        contentDescription = ""
+                    )
+
+                    Spacer(modifier = Modifier.width(2.dp))
+
+                    Text(
+                        text = "2,553",
+                        color = CampuspickTheme.colors.Gray2,
+                        style = CampuspickTheme.typography.caption3
+                    )
+                }
+            },
+            modifier = Modifier
+                .height(217.dp)
+        )
     }
 }
